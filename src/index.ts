0c02abceea20c5fe011ea911559cb03c2b146669
Chris Scott <cscott@insiderintelligence.com>
2021-10-01 15:43:47 -0400

    Fix for link.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import { IIButton, ButtonProps } from './components/button';
-import { IILink, LinkProps } from './components/link';
-import { TextInput, TextInputProps } from './components/text-input';
-import { IIForm, FormProps } from './components/form';
+// import { IIButton, ButtonProps } from './components/button';
+// import { IILink, LinkProps } from './components/link';
+export { TextInput, TextInputProps } from './components/text-input';
+// import { IIForm, FormProps } from './components/form';
 
-export { IILink, LinkProps };
-export { IIButton, ButtonProps };
-export { TextInput, TextInputProps };
-export { IIForm, FormProps };
+// export { IILink, LinkProps };
+// export { IIButton, ButtonProps };
+// export { IIForm, FormProps };
 
-export { IILogo, IILogoProps } from './components/IILogo';
+// export { IILogo, IILogoProps } from './components/IILogo';
